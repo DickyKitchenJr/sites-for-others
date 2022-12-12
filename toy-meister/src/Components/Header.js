a2a213330177d55d84adebdbf89b09f932364928
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-12-12 10:13:56 -0500

    Update Header.js

--- toy-meister/src/Components/Header.js
+++ toy-meister/src/Components/Header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import Logo from "../Images/logoCircle.png";
 
 function Header() {
   return (
     <>
-      <h1>Header</h1>
+      <h1>TOYMEiSTER</h1>
+      <img src={Logo} alt="business logo" />
     </>
   )
 }
