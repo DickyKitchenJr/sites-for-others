c332d61283819ad78cd9fd8a4a406fbebca0b34a
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-12-12 11:23:41 -0500

    created initial layout for Nav

---
+++ toy-meister/src/Components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <>
+      <nav>
+        <p>About</p>
+        <p>Location</p>
+        <p>Products</p>
+        <p>Contact</p>
+      </nav>
+    </>
+  )
+}
+
+export default Nav
