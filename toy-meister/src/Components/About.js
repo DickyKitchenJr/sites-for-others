25a03d9e73e8cc2a07d19f28395632e0604550ad
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-12-12 14:09:42 -0500

    added details and styling to About p

--- toy-meister/src/Components/About.js
+++ toy-meister/src/Components/About.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './About.css';
-import Store from '../Images/storefront.jpg';
+import React from "react";
+import "./About.css";
+import Store from "../Images/storefront.jpg";
 
 function About() {
   return (
@@ -8,9 +8,23 @@ function About() {
       <div>
         <h2>The Largest Toy Selection on the East Coast!</h2>
       </div>
-      <img className='storefront' src={Store} alt="store front" />
+      <img className="storefront" src={Store} alt="store front" />
+      <p className="aboutWriteUp">
+        Located on the second floor of Virginia Beach's Antique Mall, Toymeister
+        houses the largest toy selection on the East Coast.
+        <br />
+        Established in 19XX, Toymeister set itself apart in the toy industry by
+        providing toys ranging from antique to modern.
+        <br />
+        The 15000 square foot facility is packed to the brim with row after row
+        of all the biggest names of yesteryear and today.
+        <br />
+        Whether you are looking for a vintage action figure to complete your
+        collection or a missing piece to complete your Lego set, our friendly
+        staff can help you find the perfect toy for any occasion.
+      </p>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
