2698a3933e02dced9e9cd322cc6d5d11f0f06ce0
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-12-13 10:56:25 -0500

    Update ProductPics.js

--- toy-meister/src/Components/ProductPics.js
+++ toy-meister/src/Components/ProductPics.js
@@ -1,83 +1,83 @@
-import ArtPrints from '../Images/artprints.webp';
-import Barbie from '../Images/barbie.webp';
-import Batman from '../Images/batman.webp';
-import Cars from '../Images/cars.webp';
-import ComicBooks from '../Images/comicbooks.webp';
-import Funko from '../Images/funko.webp';
-import GI1 from '../Images/gijoe.webp';
-import GI2 from '../Images/gijoe2.webp';
+import ArtPrints from "../Images/artprints.webp";
+import Barbie from "../Images/barbie.webp";
+import Batman from "../Images/batman.webp";
+import Cars from "../Images/cars.webp";
+import ComicBooks from "../Images/comicbooks.webp";
+import Funko from "../Images/funko.webp";
+import GI1 from "../Images/gijoe.webp";
+import GI2 from "../Images/gijoe2.webp";
 import GI3 from "../Images/gijoe3.webp";
 import HotWheels from "../Images/hotwheels.webp";
-import LegoFig from '../Images/legofigures.webp';
-import Legos from '../Images/legos.webp';
-import LegoSet from '../Images/legosets.webp';
-import LooneyToons from '../Images/looneytoons.webp';
+import LegoFig from "../Images/legofigures.webp";
+import Legos from "../Images/legos.webp";
+import LegoSet from "../Images/legosets.webp";
+import LooneyToons from "../Images/looneytoons.webp";
 import Marvel from "../Images/marvel.webp";
-import MarvelDisplay from '../Images/marveldisplay.webp';
-import StarTrek from '../Images/startrek.webp';
+import MarvelDisplay from "../Images/marveldisplay.webp";
+import StarTrek from "../Images/startrek.webp";
 import StarTrek2 from "../Images/startrek2.webp";
-import Transfromers from '../Images/transformers.webp';
+import Transfromers from "../Images/transformers.webp";
 import Transfromers2 from "../Images/transformers2.webp";
 
 export const ProductPics = [
-    {
-        image: ArtPrints,
-    }, 
-    {
-        image: Barbie,
-    },
-    {
-        image:Batman,
-    },
-    {
-        image: Cars,
-    },
-    {
-        image: ComicBooks,
-    },
-    {
-        image: Funko,
-    },
-    {
-        image: GI1,
-    },
-    {
-        image:GI2,
-    },
-    {
-        image:GI3,
-    },
-    {
-        image: HotWheels,
-    },
-    {
-        image: LegoFig,
-    },
-    {
-        image: LegoSet,
-    },
-    {
-        image: Legos,
-    },
-    {
-        image: LooneyToons,
-    },
-    {
-        image: Marvel,
-    },
-    {
-        image: MarvelDisplay,
-    },
-    {
-        image: StarTrek,
-    },
-    {
-        image: StarTrek2,
-    },
-    {
-        image: Transfromers,
-    },
-    {
-        image: Transfromers2,
-    },
-]
+  {
+    image: ArtPrints,
+  },
+  {
+    image: Barbie,
+  },
+  {
+    image: Batman,
+  },
+  {
+    image: Cars,
+  },
+  {
+    image: ComicBooks,
+  },
+  {
+    image: Funko,
+  },
+  {
+    image: GI1,
+  },
+  {
+    image: GI2,
+  },
+  {
+    image: GI3,
+  },
+  {
+    image: HotWheels,
+  },
+  {
+    image: LegoFig,
+  },
+  {
+    image: LegoSet,
+  },
+  {
+    image: Legos,
+  },
+  {
+    image: LooneyToons,
+  },
+  {
+    image: Marvel,
+  },
+  {
+    image: MarvelDisplay,
+  },
+  {
+    image: StarTrek,
+  },
+  {
+    image: StarTrek2,
+  },
+  {
+    image: Transfromers,
+  },
+  {
+    image: Transfromers2,
+  },
+];
