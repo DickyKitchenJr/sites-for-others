27c1a05cfd0126cc76382d7798d535b828a498df
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-12-13 10:48:13 -0500

    Create ProductPics.js

---
+++ toy-meister/src/Components/ProductPics.js
@@ -0,0 +1,22 @@
+import ArtPrints from '../Images/artprints.webp';
+import Barbie from '../Images/barbie.webp';
+import Batman from '../Images/batman.webp';
+import Cars from '../Images/cars.webp';
+import ComicBooks from '../Images/comicbooks.webp';
+import Funko from '../Images/funko.webp';
+import GI1 from '../Images/gijoe.webp';
+import GI2 from '../Images/gijoe2.webp';
+import GI3 from "../Images/gijoe3.webp";
+import HotWheels from "../Images/hotwheels.webp";
+import LegoFig from '../Images/legofigures.webp';
+import Legos from '../Images/legos.webp';
+import LegoSet from '../Images/legosets.webp';
+import LooneyToons from '../Images/looneytoons.webp';
+import Marvel from "../Images/marvel.webp";
+import MarvelDisplay from '../Images/marveldisplay.webp';
+import StarTrek from '../Images/startrek.webp';
+import StarTrek2 from "../Images/startrek2.webp";
+import Transfromers from '../Images/transformers.webp';
+import Transfromers2 from "../Images/transformers2.webp";
+
+export const ProductPics = []
