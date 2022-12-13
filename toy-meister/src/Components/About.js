fa4d123d9edc70e6ef3f3f1dddd353cd9bed6fbe
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-12-13 09:22:09 -0500

    adjusted look of About p

--- toy-meister/src/Components/About.js
+++ toy-meister/src/Components/About.js
@@ -16,13 +16,13 @@ function About() {
       <p className="aboutWriteUp">
         Located on the second floor of Virginia Beach's Antique Mall, Toymeister
         houses the largest toy selection on the East Coast.
-        <br />
+        <br /><br/>
         Established in 19XX, Toymeister set itself apart in the toy industry by
         providing toys ranging from antique to modern.
-        <br />
+        <br /><br/>
         The 15000 square foot facility is packed to the brim with row after row
         of all the biggest names of yesteryear and today.
-        <br />
+        <br /><br/>
         Whether you are looking for a vintage action figure to complete your
         collection or a missing piece to complete your Lego set, our friendly
         staff can help you find the perfect toy for any occasion.
