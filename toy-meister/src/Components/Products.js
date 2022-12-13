48cbc6e32ef4164441ab50e15bbd6b4562cdd89d
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-12-13 11:24:17 -0500

    added picture change function

--- toy-meister/src/Components/Products.js
+++ toy-meister/src/Components/Products.js
@@ -1,14 +1,52 @@
-import React from 'react'
-import './Products.css'
+import React, { useState } from "react";
+import "./Products.css";
+import { ProductPics } from "./ProductPics";
 
 function Products() {
+  const [picCount, setPicCount] = useState(0);
+
+  const onLeftClick = () => {
+    if (picCount === 0) {
+      setPicCount(19);
+    } else {
+      setPicCount(picCount - 1);
+    }
+  };
+
+  const onRightClick = () => {
+    if (picCount === 19) {
+      setPicCount(0);
+    } else {
+      setPicCount(picCount + 1);
+    }
+  };
+
   return (
     <>
       <div>
         <h2>Thousands Of Toys To Choose From!</h2>
       </div>
+      <div className="products">
+        <button onClick={onLeftClick}>&#171;</button>
+
+        {ProductPics.map((pics) => {
+          return (
+            <img
+              className="product-pic"
+              src={pics.image}
+              alt="product"
+              key={ProductPics.indexOf(pics)}
+              style ={{
+                display: picCount !== ProductPics.indexOf(pics) ? 'none' : 'block'
+              }}
+            />
+          );
+        })}
+
+        <button onClick={onRightClick}>&#187;</button>
+      </div>
     </>
   );
 }
 
-export default Products
+export default Products;
